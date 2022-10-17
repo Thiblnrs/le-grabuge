3e0bf98d92649d305dafd8782663535218c767aa
Trasher <tllinares.pro@gmail.com>
2022-10-17 15:42:33 +0200

    Newsletter dans footer

--- footer.php
+++ footer.php
@@ -12,16 +12,22 @@
 ?>
 
 	<footer>
+		 <!-- Newsletter--------------------------------------->
+    <section id="newsletter">
+        <div>
+            <h2>Newsletter</h2>
+            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre  programmation et bien d’autres choses. 10 % de réduction sur votre premiere visite chez nous en vous inscrivant.</p>
+            <?php echo do_shortcode('[mailpoet_form id="1"]') ?>
+        </div>
+    </section>
 			<div>
 			<nav id="mentionfooter">
-				<ul>
-					<li><a href="#">Newsletter</a></li>
-					<li><a href="#">Mentions légales</a></li>
-					<li><a href="#">Espace pro</a></li>
-					<li><a href="#">Politique de confidentialités</a></li>
-					<li><a href="#">CGV</a></li>
-					<li><a href="#">CGU</a></li>
-				</ul>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'menu-secondaire',
+					'container' => '',
+					'menu_class' => '',
+					)
+				) ?>
 			</nav>
 			</div>
 		</footer>
--- front-page.php
+++ front-page.php
@@ -184,14 +184,7 @@ get_header();
         <div><a href="#">Plus d’article</a></div>
     </section>
 
-    <!-- Newsletter--------------------------------------->
-    <section id="newsletter">
-        <div>
-            <h2>Newsletter</h2>
-            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre  programmation et bien d’autres choses. 10 % de réduction sur votre premiere visite chez nous en vous inscrivant.</p>
-            <?php echo do_shortcode('[mailpoet_form id="1"]') ?>
-        </div>
-    </section>
+   
 
     <!-- citation--------------------------------------->
     <section id="citation">
