149f6c212c68964434ad4ee368ff7f15da25ffb5
Trasher <tllinares.pro@gmail.com>
2022-10-17 14:49:59 +0200

    Cration template part archive concert

--- archive-concert.php
+++ archive-concert.php
@@ -28,26 +28,42 @@ get_header();
 						$mois = $dateTime->format('F');
 					}
 				?>
-
-				<?php
-					if ($mois == "October") :
-				?>
+				<?php if ($mois == "January") : ?>
+					<h3 class="moisJanvier">Janvier</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "February") : ?>
+					<h3 class="moisFevrier">Février</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "March") : ?>
+					<h3 class="moisMars">Mars</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "April") : ?>
+					<h3 class="moisAvril">Avril</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "May") : ?>
+					<h3 class="moisMai">Mai</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "June") : ?>
+					<h3 class="moisJuin">Juin</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "July") : ?>
+					<h3 class="moisJuillet">Juillet</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "August") : ?>
+					<h3 class="moisAout">Aout</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "September") : ?>
+					<h3 class="moisSeptembre">Septembre</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "October") : ?>
 					<h3 class="moisOctobre">Octobre</h3>
-					<div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
-						<p>
-                            <?php 
-								$dateFR = strtotime( get_field('date') );
-								echo date_i18n( "j M Y", $dateFR ); ?><br />
-                            <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-                        </p>
-						<h4><?php the_title() ?></h4>
-						<a href="<?php the_permalink() ?>">En savoir plus</a>
-					</div>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
 				<?php elseif ($mois == "November") : ?>
 					<h3 class="moisNovembre">Novembre</h3>
-					<div>
-						<?php the_title() ?>
-					</div>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+				<?php elseif ($mois == "December") : ?>
+					<h3 class="moisDecembre">Décembre</h3>
+					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
 			<?php endif; endwhile;
 
 			// the_posts_navigation();
