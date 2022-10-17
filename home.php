b70d7176fa03fab0b35abbb5a2f07dfc48eeac19
Trasher <tllinares.pro@gmail.com>
2022-10-17 13:06:18 +0200

    Création page actu

--- home.php
+++ home.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying archive pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -49,7 +44,8 @@ get_header();
 		endif;
 		?>
 
-	</main><
+	</main><!-- #main -->
 
 <?php
+get_sidebar();
 get_footer();
