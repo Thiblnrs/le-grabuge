c5d1ca2ba7d7fb3a3e9b537d90b03c4dfea8e497
Trasher <tllinares.pro@gmail.com>
2022-10-17 14:52:18 +0200

    Fix template part

--- archive-concert.php
+++ archive-concert.php
@@ -30,40 +30,40 @@ get_header();
 				?>
 				<?php if ($mois == "January") : ?>
 					<h3 class="moisJanvier">Janvier</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "February") : ?>
 					<h3 class="moisFevrier">Février</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "March") : ?>
 					<h3 class="moisMars">Mars</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "April") : ?>
 					<h3 class="moisAvril">Avril</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "May") : ?>
 					<h3 class="moisMai">Mai</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "June") : ?>
 					<h3 class="moisJuin">Juin</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "July") : ?>
 					<h3 class="moisJuillet">Juillet</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "August") : ?>
 					<h3 class="moisAout">Aout</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "September") : ?>
 					<h3 class="moisSeptembre">Septembre</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "October") : ?>
 					<h3 class="moisOctobre">Octobre</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "November") : ?>
 					<h3 class="moisNovembre">Novembre</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 				<?php elseif ($mois == "December") : ?>
 					<h3 class="moisDecembre">Décembre</h3>
-					<?php get_template_part(('template-parts/concert/archive-concert.php')) ?>
+					<?php get_template_part('template-parts/concert/archive-concert') ?>
 			<?php endif; endwhile;
 
 			// the_posts_navigation();
