7ea9a2ff49a227471d60fb8a71e0c56abd1ee31c
Trasher <tllinares.pro@gmail.com>
2022-10-17 13:08:59 +0200

    Structure home

--- home.php
+++ home.php
@@ -10,42 +10,22 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+	<main>
+		<h1><?php the_archive_title() ?></h1>
+		<div class="grilleActu">
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
-				the_post();
+				the_post(); ?>
+				<div class="actualite">
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
+				</div>
+			<?php endwhile;
 
 			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
 		?>
-
+		</div>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
