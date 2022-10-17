dfb418cc0fa978aeda4fdca319c9cd1270ee1e35
Trasher <tllinares.pro@gmail.com>
2022-10-17 15:38:08 +0200

    Mise en place shortcode newsletter home

--- front-page.php
+++ front-page.php
@@ -188,11 +188,8 @@ get_header();
     <section id="newsletter">
         <div>
             <h2>Newsletter</h2>
-            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre  programmation et bien d’autres choses. 10 % de réduction
-sur votre premiere visite chez nous en vous inscrivant.</p>
-            <p>J’accepte de recevoir les news du grabuge</p>
-            <hr />
-            <div><a href="">S'inscrire</a></div>
+            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre  programmation et bien d’autres choses. 10 % de réduction sur votre premiere visite chez nous en vous inscrivant.</p>
+            <?php echo do_shortcode('[mailpoet_form id="1"]') ?>
         </div>
     </section>
 
