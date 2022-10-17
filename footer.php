f2404f8305602bced8375f2433e3073eff70392f
Trasher <tllinares.pro@gmail.com>
2022-10-17 19:05:22 +0200

    nav footer test

--- footer.php
+++ footer.php
@@ -11,27 +11,40 @@
 
 ?>
 
-	<footer>
-		 <!-- Newsletter--------------------------------------->
+<footer>
+    <!-- Newsletter--------------------------------------->
     <section id="newsletter">
         <div>
             <h2>Newsletter</h2>
-            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre  programmation et bien d’autres choses. 10 % de réduction sur votre premiere visite chez nous en vous inscrivant.</p>
+            <p>Inscriver vous à notre news letter pour ne rater aucune information sur notre programmation et bien
+                d’autres choses. 10 % de réduction sur votre premiere visite chez nous en vous inscrivant.</p>
             <?php echo do_shortcode('[mailpoet_form id="1"]') ?>
         </div>
     </section>
-			<div>
-			<nav id="mentionfooter">
-				<?php wp_nav_menu( array(
+    <div>
+        <nav id="mentionfooter">
+            <?php wp_nav_menu( array(
+					'theme_location' => 'menu-principal',
+					'container' => '',
+					'menu_class' => '',
+					)
+				) ?>
+        </nav>
+    </div>
+
+    <div>
+        <nav id="mentionfooter">
+            <?php wp_nav_menu( array(
 					'theme_location' => 'menu-secondaire',
 					'container' => '',
 					'menu_class' => '',
 					)
 				) ?>
-			</nav>
-			</div>
-		</footer>
+        </nav>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
