7cd6daa7f0d6f5acc451e8c5bd7395a595622b30
Trasher <tllinares.pro@gmail.com>
2022-10-23 18:10:27 +0200

    bg

--- single-concert.php
+++ single-concert.php
@@ -79,7 +79,7 @@ get_header();
     </section>
 
     <!-- Proposition d'artiste--------------------------------------->
-    <section id="prochain-artiste" class="">
+    <section id="prochain-artiste" class="bg-black-">
         <div>
             <?php $args = array(
 							'post_type' => 'concert',
