974c43a37c5bc37c963aa18d6b0f335f44598c8b
Trasher <tllinares.pro@gmail.com>
2022-10-23 17:54:02 +0200

    modif

--- single-concert.php
+++ single-concert.php
@@ -119,31 +119,6 @@ get_header();
                     <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
                 </div>
             </div>
-
-            <div class="cont-artiste-2">
-                <div>
-                    <?php $photo_artiste = get_field('photo_artiste') ?>
-                    <img src="<?php echo $photo_artiste['sizes']['medium'] ?>"
-                        alt="<?php echo $photo_artiste['alt'] ?>" />
-                    <div class="cover-date">
-                        <p>
-                            <?php 
-								$dateFR = strtotime( get_field('date') );
-								echo date_i18n( "j M Y", $dateFR ); ?><br />
-                            <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-                        </p>
-                        <p>Techno</p>
-                    </div>
-
-                    <hr class="hr-desc" />
-
-                    <h3><?php the_title() ?><br />
-                        <span><?php the_field('premiere-partie') ?></span>
-                    </h3>
-
-                    <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
-                </div>
-            </div>
             <?php endwhile;
 				endif;
 				
