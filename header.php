23ca3129d2fb30f356556f42c227f41142c8e4a8
Trasher <tllinares.pro@gmail.com>
2022-11-30 09:48:42 +0100

    fix photo artiste header

--- header.php
+++ header.php
@@ -12,53 +12,54 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<header>
-			<div>
-				<?php the_custom_logo( ) ?>
-				<h1>Le grabuge</h1>
-			</div>
-			<!--NAV--------------------------------------->
-			<nav>
-				<ul>
-					<li><a href="#">Programmation</a></li>
-					<li class="btn-menu"><a href="#">Menu</a></li>
-				</ul>
-			</nav>
-			<div class="menu">
-				<section id="fermer">
-					<a href="#">Fermer</a>
-				</section>
+    <?php wp_body_open(); ?>
+    <header>
+        <div>
+            <?php the_custom_logo( ) ?>
+            <h1>Le grabuge</h1>
+        </div>
+        <!--NAV--------------------------------------->
+        <nav>
+            <ul>
+                <li><a href="#">Programmation</a></li>
+                <li class="btn-menu"><a href="#">Menu</a></li>
+            </ul>
+        </nav>
+        <div class="menu">
+            <section id="fermer">
+                <a href="#">Fermer</a>
+            </section>
 
-				<section id="menu-contenu">
-					<div>
-						<nav id="menu-principal">
-							<?php wp_nav_menu( array(
+            <section id="menu-contenu">
+                <div>
+                    <nav id="menu-principal">
+                        <?php wp_nav_menu( array(
 								'theme_location' => 'menu-principal',
 								'container' => '',
 								'menu_class' => '',
 									)
 								) ?>
-						</nav>
-						<nav id="mention">
-							<?php wp_nav_menu( array(
+                    </nav>
+                    <nav id="mention">
+                        <?php wp_nav_menu( array(
 								'theme_location' => 'menu-secondaire',
 								'container' => '',
 								'menu_class' => '',
 									)
 								) ?>
-						</nav>
-					</div>
-					<?php $args = array(
+                    </nav>
+                </div>
+                <?php $args = array(
 							'post_type' => 'concert',
 							'posts_per_page' => 1,
 							'meta_key'  => 'date',
@@ -70,34 +71,36 @@
 				
 				if ( $concertsAVenir->have_posts() ) :
 					while ( $concertsAVenir->have_posts() ) : $concertsAVenir->the_post(); ?>
-						
-				<div class="cont-artiste-menu">
-					<div>
-						<img src="<?php the_field('photo_artiste') ?>" alt="" />
-						<div class="cover-date-menu">
-							<p>
-								<?php 
+
+                <div class="cont-artiste-menu">
+                    <div>
+                        <?php $photo = get_field('photo_artiste') ?>
+                        <img src="<?php echo $photo['sizes']['large'] ?>" alt="<?php echo $photo['alt'] ?>" />
+                        <div class="cover-date-menu">
+                            <p>
+                                <?php 
 								$dateFR = strtotime( get_field('date') );
 								echo date_i18n( "j M Y", $dateFR ); ?><br />
-								<?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-							</p>
-							<p>Techno</p>
-						</div>
+                                <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+                            </p>
+                            <p>Techno</p>
+                        </div>
 
-						<hr class="hr-desc" />
+                        <hr class="hr-desc" />
 
-						<h3><?php the_title() ?><br />
-						<span><?php the_field('premiere-partie') ?></span></h3>
+                        <h3><?php the_title() ?><br />
+                            <span><?php the_field('premiere-partie') ?></span>
+                        </h3>
 
-						<div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
-						<div class="cta-b"><a href="">Billetterie</a></div>
-					</div>
-				</div>
-					<?php endwhile;
+                        <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
+                        <div class="cta-b"><a href="">Billetterie</a></div>
+                    </div>
+                </div>
+                <?php endwhile;
 				endif;
 				
 				wp_reset_postdata(); ?>
-				</section>
-			</div>
-		</header>
-		<!--FIN HEADER--------------------------------------->
+            </section>
+        </div>
+    </header>
+    <!--FIN HEADER--------------------------------------->
