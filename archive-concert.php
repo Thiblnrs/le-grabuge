ecc18e4222356324af94d3188d031ebc532ee771
Trasher <tllinares.pro@gmail.com>
2022-12-07 09:51:04 +0100

    div detail concert

--- archive-concert.php
+++ archive-concert.php
@@ -72,6 +72,20 @@ get_header();
 			// the_posts_navigation();
 		?>
     </div>
+
+    <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
+    <div>
+    <p>
+        <?php 
+            $dateFR = strtotime( get_field('date') );
+            echo date_i18n( "j M Y", $dateFR ); ?><br />
+        <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+    </p>
+    <h4><?php the_title() ?></h4>
+    <hr>
+    <a href="<?php the_permalink() ?>">En savoir plus</a>
+    </div>
+</div>
 </main><!-- #main -->
 
 <?php
