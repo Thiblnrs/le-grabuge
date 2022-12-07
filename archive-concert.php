322c91d210fabf5706a0c9257f50f69d533ce818
Trasher <tllinares.pro@gmail.com>
2022-12-07 10:13:35 +0100

    marche stp

--- archive-concert.php
+++ archive-concert.php
@@ -74,6 +74,19 @@ get_header();
     </div>
 
     <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
+    <div>
+    <p>
+        <?php 
+            $dateFR = strtotime( get_field('date') );
+            echo date_i18n( "j M Y", $dateFR ); ?>
+            </p></div>
+           <div><p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+    </p></div>
+    <hr>
+    <h4><?php the_title() ?></h4>
+     <button> <a href="<?php the_permalink() ?>">Billetterie</a></button>
+   <button> <a href="<?php the_permalink() ?>">En savoir plus</a></button>
+</div>
 </div>
 </main><!-- #main -->
 
