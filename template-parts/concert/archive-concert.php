942cb86fdc1bfe9eda860b8e8fc395693e64ec35
Trasher <tllinares.pro@gmail.com>
2022-12-07 14:57:02 +0100

    nouveau test prog

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,8 +1,10 @@
 <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
-<div class="cover-date">
-                        <?php 
-								$dateFR = strtotime( get_field('date') );
-							?>
+ <div class="cont-artiste">
+                <div>
+                    <?php $photo_artiste = get_field('photo_artiste') ?>
+                    <img src="<?php echo $photo_artiste['sizes']['medium'] ?>"
+                        alt="<?php echo $photo_artiste['alt'] ?>" />
+                    <div class="cover-date">
                         <p>
                             <?php 
 								$dateFR = strtotime( get_field('date') );
@@ -11,9 +13,14 @@
                         </p>
                         <p><?php the_field("style_")?></p>
                     </div>
+
+                    <hr class="hr-desc" />
+
+                    <h3><?php the_title() ?><br />
+                        <span><?php the_field('premiere-partie') ?></span>
+                    </h3>
+
                     <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
-                    <div class="cta-b"><a href="#">Billetterie</a></div>
+                    <div class="cta-b"><a href="">Billetterie</a></div>
                 </div>
             </div>
-        </div>
-         </div>
