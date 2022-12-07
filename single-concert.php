d9358658e12183f89be67915bdf6322db0e7ab7f
Trasher <tllinares.pro@gmail.com>
2022-12-07 14:01:13 +0100

    testage fermeture ouverture

--- single-concert.php
+++ single-concert.php
@@ -18,6 +18,7 @@ get_header();
 								$dateFR = strtotime( get_field('date') );
 								echo date_i18n( "j M Y", $dateFR ); ?><br />
                     <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+                    <?php the_field('heure_douverture') ?> - <?php the_field('heure_de_fermeture') ?>
                 </p>
                 <p><?php the_field("style_")?></p> 
             </div>
