66f0a2900b8977cf0ed820b343e35d9b9facc466
Trasher <tllinares.pro@gmail.com>
2022-12-07 09:36:11 +0100

    supprimer prefix archive

--- functions.php
+++ functions.php
@@ -174,3 +174,17 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/* Retirer les préfixes sur les pages d'archives */
+function wp_retirer_prefix_dans_archives() {
+	if (is_category()) {
+			$title = single_cat_title('', false);
+		} elseif (is_tag()) {
+			$title = single_tag_title('', false);
+		} elseif (is_author()) {
+			$title = '<span class="vcard">' . get_the_author() . '</span>' ;
+		} elseif (is_post_type_archive()) {
+			 $title = post_type_archive_title('', false);
+		}
+	return $title;
+}
+add_filter('get_the_archive_title', 'wp_retirer_prefix_dans_archives');
