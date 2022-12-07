de18a3e03a2bb3c406b7cd916d34469539e14d64
Trasher <tllinares.pro@gmail.com>
2022-12-07 15:00:14 +0100

    teste prog 5

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,26 +1,16 @@
 <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
- <div class="cont-artiste">
-                <div>
-                    <?php $photo_artiste = get_field('photo_artiste') ?>
-                    <img src="<?php echo $photo_artiste['sizes']['medium'] ?>"
-                        alt="<?php echo $photo_artiste['alt'] ?>" />
-                    <div class="cover-date">
-                        <p>
-                            <?php 
+<div>
+    <p>
+         <?php 
 								$dateFR = strtotime( get_field('date') );
 								echo date_i18n( "j M Y", $dateFR ); ?><br />
-                            <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-                        </p>
-                        <p><?php the_field("style_")?></p>
-                    </div>
-
-                    <hr class="hr-desc" />
-
-                    <h3><?php the_title() ?><br />
-                        <span><?php the_field('premiere-partie') ?></span>
-                    </h3>
-
-                    <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
+            </p></div>
+           <div><p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+    </p>
+<p><?php the_field("style_")?></p></div>
+    <hr class="hr-desc" />
+    <h4><?php the_title() ?>
+ <span><?php the_field('premiere-partie') ?></span></h4>
+  <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
                     <div class="cta-b"><a href="">Billetterie</a></div>
-                </div>
-            </div>
+</div>
