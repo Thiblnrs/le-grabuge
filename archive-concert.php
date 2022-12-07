cd2b07a067b3059ac72086550bcb34851b061324
Trasher <tllinares.pro@gmail.com>
2022-12-07 10:07:33 +0100

    test 36500

--- archive-concert.php
+++ archive-concert.php
@@ -74,23 +74,6 @@ get_header();
     </div>
 
     <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
-    <div>
-    <p>
-        <?php 
-            $dateFR = strtotime( get_field('date') );
-            echo date_i18n( "j M Y", $dateFR ); ?>
-            
-            </p>
-            <p>
-        <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-    </p>
-    </div>
-    <hr>
-    <div>
-    <h4><?php the_title() ?></h4>
-    
-    <a href="<?php the_permalink() ?>">En savoir plus</a>
-    </div>
 </div>
 </main><!-- #main -->
 
--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,10 +1,13 @@
 <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
+<div>
     <p>
         <?php 
             $dateFR = strtotime( get_field('date') );
-            echo date_i18n( "j M Y", $dateFR ); ?><br />
-        <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-    </p>
+            echo date_i18n( "j M Y", $dateFR ); ?>
+            </p></div>
+            <br/>
+           <div><p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+    </p></div>
     <hr>
     <h4><?php the_title() ?></h4>
     <a href="<?php the_permalink() ?>">En savoir plus</a>
