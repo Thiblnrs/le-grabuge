0683ef17ca78a721a4c3e08e8e92e570f8485c27
Trasher <tllinares.pro@gmail.com>
2022-12-07 13:52:52 +0100

    style

--- single-concert.php
+++ single-concert.php
@@ -118,7 +118,7 @@ get_header();
 								echo date_i18n( "j M Y", $dateFR ); ?><br />
                             <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
                         </p>
-                        <p>Techno</p>
+                        <p><?php the_field("style_")?></p>
                     </div>
 
                     <hr class="hr-desc" />
