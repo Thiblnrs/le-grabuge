56847e425fb1c6d1fee6166e8cda7f8fc6da2dcd
Trasher <tllinares.pro@gmail.com>
2022-12-07 14:52:05 +0100

    testage prog

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,14 +1,18 @@
 <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
-<div>
-    <p>
-        <?php 
-            $dateFR = strtotime( get_field('date') );
-            echo date_i18n( "j M Y", $dateFR ); ?>
-            </p></div>
-           <div><p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-    </p></div>
-    <hr>
-    <h4><?php the_title() ?></h4>
-     <button> <a href="<?php the_permalink() ?>">Billetterie</a></button>
-   <button> <a href="<?php the_permalink() ?>">En savoir plus</a></button>
-</div>
+<div class="cover-date">
+                        <?php 
+								$dateFR = strtotime( get_field('date') );
+							?>
+                        <p>
+                            <?php 
+								$dateFR = strtotime( get_field('date') );
+								echo date_i18n( "j M Y", $dateFR ); ?><br />
+                            <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+                        </p>
+                        <p><?php the_field("style_")?></p>
+                    </div>
+                    <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
+                    <div class="cta-b"><a href="#">Billetterie</a></div>
+                </div>
+            </div>
+        </div>
