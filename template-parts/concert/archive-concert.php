b79ad813cf87ba4dbd24d5e0d7ce43188714b6de
Trasher <tllinares.pro@gmail.com>
2022-12-07 16:08:22 +0100

    lien concert page prog

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,25 +1,29 @@
 <div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert">
-    <div class="bg-gris">
-    <div>
-        <p>
-            <?php 
-                                    $dateFR = strtotime( get_field('date') );
-                                    echo date_i18n( "j M Y", $dateFR ); ?><br />
-                </p></div>
-            <div><p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-        </p>
-        <p><?php the_field("style_")?></p></div>
-        <hr class="hr-desc" />
-        <h4><?php the_title() ?> <br>
-            <?php while( have_rows('line_up') ) : the_row(); ?>
-                <?php the_sub_field('nom_artiste') ?> <br>
-            <?php endwhile; ?>
-        </h4>
-        <div class="cta-esp">
-            <a href="<?php the_permalink() ?>">En savoir plus</a>
+    <a href="<?php the_permalink() ?>">
+        <div class="bg-gris">
+        <div>
+            <p>
+                <?php 
+                    $dateFR = strtotime( get_field('date') );
+                    echo date_i18n( "j M Y", $dateFR ); ?><br />
+            </p>
         </div>
-        <!-- <div class="cta-b">
-            <a href="">Billetterie</a>
-        </div> -->
-    </div>
+        <div>
+            <p><?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+            </p>
+            <p><?php the_field("style_")?></p></div>
+            <hr class="hr-desc" />
+            <h4><?php the_title() ?> <br>
+                <?php while( have_rows('line_up') ) : the_row(); ?>
+                    <?php the_sub_field('nom_artiste') ?> <br>
+                <?php endwhile; ?>
+            </h4>
+            <!-- <div class="cta-esp">
+                <a href="<?php the_permalink() ?>">En savoir plus</a>
+            </div> -->
+            <div class="cta-b">
+                <a href="">Billetterie</a>
+            </div>
+        </div>
+    </a>
 </div>
