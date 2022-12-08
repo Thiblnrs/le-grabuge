984d4ba088a0fa9becda82cecb8c7dedf05bed5e
Trasher <tllinares.pro@gmail.com>
2022-12-08 20:52:52 +0100

    ok

--- assets/script/main.js
+++ assets/script/main.js
@@ -55,16 +55,3 @@ jQuery(document).ready(function ($) {
 		$(this).next().toggleClass("ouvert");
 	});
 });
-
-	//test apparition
-	const alllinks = document.querySelectorAll("ul li");
-
-	alllinks.forEach((link) => {
-		const location = link.getAttribute("data-loc");
-
-		link.addEventListener("mouseenter", () => {
-			cursor.classlist.add("hovered");
-			cursor.style.backgroundImage = "url(/assets/img/${location}.webp";
-		});
-	});
-
