ce5bc8e0429563ebab981259ebf80ed51f7e6900
Trasher <tllinares.pro@gmail.com>
2022-12-08 15:22:17 +0100

    test JS accordeon

--- assets/script/main.js
+++ assets/script/main.js
@@ -51,17 +51,22 @@ jQuery(document).ready(function ($) {
 	});
 });
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(".accordion").click(function () {
+	$(this).addClass("acctive");
+	$(this).next().toggleClass("ouvert");
+});
 
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function () {
-		this.classList.toggle("acctive");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight) {
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		}
-	});
-}
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+// 	acc[i].addEventListener("click", function () {
+// 		this.classList.toggle("acctive");
+// 		var panel = this.nextElementSibling;
+// 		if (panel.style.maxHeight) {
+// 			panel.style.maxHeight = null;
+// 		} else {
+// 			panel.style.maxHeight = panel.scrollHeight + "px";
+// 		}
+// 	});
+// }
