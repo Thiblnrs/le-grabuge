5ffb9bdb9cdc24c6c85df104c39f00ac931bb70c
Trasher <tllinares.pro@gmail.com>
2022-12-08 09:02:19 +0100

    fix classes archive concert

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -1,4 +1,4 @@
-<div style="background-image:url(<?php the_post_thumbnail_url() ?>)" class="detailConcert <?php post_class(); ?>">
+<div style="background-image:url(<?php the_post_thumbnail_url() ?>)" <?php post_class('detailConcert'); ?>>
         <div class="bg-gris">
         <div>
             <p>
