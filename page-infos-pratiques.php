7135ffb2b384b8e5c4e03a492ba8cfa965a795c6
Trasher <tllinares.pro@gmail.com>
2022-12-08 14:56:51 +0100

    div

--- page-infos-pratiques.php
+++ page-infos-pratiques.php
@@ -43,6 +43,21 @@ get_header();
 
     <section id="faq">
         <h2>FAQ</h2>
+        
+        <div><button class="accordion">Section 1</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div></div>
     </section>
 </main>
 
