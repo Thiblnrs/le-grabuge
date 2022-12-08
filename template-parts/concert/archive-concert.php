798c2d392bd70c813d9979d73d877bf8a1304fed
Trasher <tllinares.pro@gmail.com>
2022-12-08 09:56:28 +0100

    -

--- template-parts/concert/archive-concert.php
+++ template-parts/concert/archive-concert.php
@@ -17,7 +17,7 @@
             <h4><?php the_title() ?> <br>
              - <span><?php while( have_rows('line_up') ) : the_row(); ?>
                     <?php the_sub_field('nom_artiste') ?>
-                <?php endwhile; ?></span> -
+                <?php endwhile; ?> - </span> 
             </h4>
             <div class="cta-esp">
                 <a href="<?php the_permalink() ?>">En savoir plus</a>
