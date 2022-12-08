e43395c15a24a3117275ca56489434e48da4f4db
Trasher <tllinares.pro@gmail.com>
2022-12-08 18:34:10 +0100

    gooooo

--- page-a-propos.php
+++ page-a-propos.php
@@ -15,7 +15,46 @@
 get_header();
 ?>
 
-	<main >
+	<main>
+
+	<h1>Notre histoire</h1>
+
+<!-- SLIDE / SLICK--------------------------------------->
+    <section id="diapo">
+        <div class="multiple-items">
+            <?php 
+                $galeries = get_field('galerie');
+                if( $galeries ): ?>
+            <?php foreach( $galeries as $galerie ): ?>
+            <img src="<?php echo esc_url($galerie['url']); ?>" alt="<?php echo esc_attr($galerie['alt']); ?>" />
+            <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </section>
+
+	 <!-- citation--------------------------------------->
+    <section id="citation">
+        <?php 
+            if( have_rows('citations') ):
+                while( have_rows('citations') ) : the_row(); ?>
+        <div>
+            <p><?php the_sub_field('texte_citation') ?></p>
+        </div>
+        <?php   endwhile;
+                else :
+            endif;
+        ?>
+    </section>
+
+    <!-- Raccourcis--------------------------------------->
+    <nav id="raccourcis">
+        <?php wp_nav_menu( array(
+								'theme_location' => 'menu-principal',
+								'container' => '',
+								'menu_class' => '',
+									)
+								) ?>
+    </nav>
 
 	</main><!-- #main -->
 
