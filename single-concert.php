1fd24d9817c42711fbae1f5deecaa6ee773589cf
Trasher <tllinares.pro@gmail.com>
2022-12-08 13:29:43 +0100

    hr

--- single-concert.php
+++ single-concert.php
@@ -79,7 +79,7 @@ get_header();
             </div>
             <div id="suivre-artiste">
                 <p>Suivre l'artiste</p>
-                <hr>
+                <hr class="hr-suivre-a">
                 <?php while( have_rows('reseaux_sociaux') ) : the_row(); ?>
                     <a href="<?php the_sub_field('url')  ?>"  target="_blank">
                         <?php the_sub_field('choix_reseaux')  ?>
