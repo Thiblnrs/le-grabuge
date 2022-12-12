0be825cf877bd2026b83e4a063ff3fce649593fb
Trasher <tllinares.pro@gmail.com>
2022-12-12 22:03:35 +0100

    dssdsdsds

--- front-page.php
+++ front-page.php
@@ -87,29 +87,38 @@ get_header();
 					while ( $concertsAVenir->have_posts() ) : $concertsAVenir->the_post(); ?>
 
             <div class="cont-artiste">
+            
+
+
+
+
                 <div>
-                    <?php $photo_artiste = get_field('photo_artiste') ?>
-                    <img src="<?php echo $photo_artiste['sizes']['medium'] ?>"
-                        alt="<?php echo $photo_artiste['alt'] ?>" />
-                    <div class="cover-date">
-                        <p>
-                            <?php 
-								$dateFR = strtotime( get_field('date') );
-								echo date_i18n( "j M Y", $dateFR ); ?><br />
-                            <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
-                        </p>
-                        <p><?php the_field("style_")?></p>
-                    </div>
+            <p> 
+                <?php 
+                $dateFR = strtotime( get_field('date') ); echo date_i18n( "j M Y", $dateFR ); ?> 
+            </p>
+            <p>
+                <?php $genre = get_the_term_list( $post->ID, 'genre', '', ' - ', '' ); $genre = strip_tags( $genre );
+                            echo $genre ?>
+            </p>
+            <p>
+                <?php the_field('heure_de_debut') ?> - <?php the_field('heure_de_fin') ?>
+            </p>
+          </div>
 
-                    <hr class="hr-desc" />
-
-                    <h3><?php the_title() ?><br />
-                        <span><?php the_field('premiere-partie') ?></span>
-                    </h3>
-
-                    <div class="cta-esp"><a href="<?php the_permalink() ?>">En savoir plus</a></div>
-                    <div class="cta-b"><a href="">Billetterie</a></div>
-                </div>
+            <hr class="hr-desc" />
+            <h4><?php the_title() ?> <br>
+             <span><?php while( have_rows('line_up') ) : the_row(); ?>
+                    <?php the_sub_field('nom_artiste') ?><br>
+                <?php endwhile; ?></span> 
+            </h4>
+            <div class="cta-esp">
+                <a href="<?php the_permalink() ?>">En savoir plus</a>
+            </div>
+            <div class="cta-b">
+                <a href="">Billetterie</a>
+            </div>
+                
             </div>
             <?php endwhile;
 				endif;
